777b4d1b3c5d7305b264a1705ce5dfb4fd97a026
Priyakatariya <priyakatariya2007@gmail.com>
2025-02-14 10:09:15 +0530

    Add files via upload

---
+++ PYQ.js
@@ -0,0 +1,100 @@
+
+        const paperLinksData = {
+            "mains_2025": [
+                { name: "22 Jan Shift 1", url: "https://drive.google.com/file/d/1SCuqThv9OrJsM-uz-vir5HOxkjlBCITi/view?usp=sharing" },
+                { name: "22 Jan Shift 2", url: "https://drive.google.com/file/d/1EXDGNiAFCoXHaXBGHVBuN6gJzkVtikNf/view?usp=sharing" },
+                { name: "23 Jan Shift 1", url: " https://drive.google.com/file/d/1xGOOQ_kJcP8I4YG66KfHLukk_IxAxkiV/view?usp=sharing" },
+                { name: "23 Jan Shift 2", url: " https://drive.google.com/file/d/1tSi3OfcZww8FEELqTc_EYCMWOthfbr2W/view?usp=sharing" },
+                { name: "24 Jan Shift 1", url: " https://drive.google.com/file/d/1okvc2t19W7ipZE7A8nJ91OXX2SYVrUZt/view?usp=sharing" },
+                { name: "24 Jan Shift 2", url: " https://drive.google.com/file/d/18gxZudFav1LI_UhCy1OvsSI5A3QXGLrm/view?usp=sharing" },
+                { name: "28 Jan Shift 1", url: "https://drive.google.com/file/d/15mHlzoyUCKL5EyE9Ke0vxii_hElQL8_C/view?usp=sharing " },
+                { name: "28 Jan Shift 2", url: " https://drive.google.com/file/d/1idwF32inN0phCply57hEE_5wgUf9M6z3/view?usp=sharing" },
+                { name: "29 Jan Shift 1", url: " https://drive.google.com/file/d/1nTwUJ_7zqvWc3II-WZgtCFjbw111vmLt/view?usp=sharing" },
+                { name: "29 Jan Shift 2", url: "https://drive.google.com/file/d/1TFS_mEVMFxwV9n5GGr7mQhLbhlFoHHIS/view?usp=sharing " }
+                
+
+            ],
+            "mains_2024": [
+                { name: "27 Jan Shift 1", url: "https://drive.google.com/file/d/1-wdKxlesVs_QFkCqKGsD_fQlzQjFcdnH/view?usp=sharing" },
+                { name: "27 Jan Shift 2", url: " https://drive.google.com/file/d/152DbdxixmtUwBJJ-UtADCvO8wVPxJcZl/view?usp=sharing" },
+                { name: "29 Jan Shift 1", url: " https://drive.google.com/file/d/1sXZko6l5kzMHhGQDafDRRS9a07OFateH/view?usp=sharing" },
+                { name: "29 Jan Shift 2", url: "https://drive.google.com/file/d/1PTgSZV8TMPcnWDO-0W33AuX6oSZe6HfR/view?usp=sharing" },
+                { name: "30 Jan Shift 1", url: "https://drive.google.com/file/d/1GUD1oORrilgLN6wtNt8CzWVRlZecIUOC/view?usp=sharing " },     
+                { name: "30 Jan Shift 2", url: "https://drive.google.com/file/d/1ma1FxjJa3ZYwGlmcOi3zweRgtYjV7gxO/view?usp=sharing" },
+                { name: "31 Jan Shift 1", url: " https://drive.google.com/file/d/1dprC6kH74hITrqAxEg8QvSktsB0A_HjO/view?usp=sharing" },                
+                { name: "31 Jan Shift 2", url: "https://drive.google.com/file/d/1zFWJi00C3Fweusixago3A-1yl1LsqFPr/view?usp=sharing " },
+                { name: "01 Feb Shift 1", url: "https://drive.google.com/file/d/11TKs1Sf7flPoF2i8KElBsRGjm_Lk3Jm8/view?usp=sharing" },
+                { name: "01 Feb Shift 2", url: " https://drive.google.com/file/d/1a-M47RCdSI6JYikiWmqG5Lnt0C8sLi_r/view?usp=sharing" },              
+                { name: "04 April Shift 1", url: "https://drive.google.com/file/d/1C3eYdfTXtkUyxjQSIdiW89U5-KIFrEOF/view?usp=sharing" },
+                { name: "04 April Shift 2", url: " https://drive.google.com/file/d/1szZ2LieKVHhLVyksv3p5ldN_XB0Fo98s/view?usp=sharing" },   
+                { name: "05 April Shift 1", url: "https://drive.google.com/file/d/1EE_vPu11xciFvd1zMgkYYmSg0uJr5Hbe/view?usp=sharing" },
+                { name: "05 April Shift 2", url: " https://drive.google.com/file/d/1BoG_ezW45gwQgRvD7bJVkAGyNmgush47/view?usp=sharing" },   
+                { name: "06 April Shift 1", url: "https://drive.google.com/file/d/1C6DF8Q8fSAEwddRPGnzPxKm5qfnJQh7A/view?usp=sharing" },
+                { name: "06 April Shift 2", url: " https://drive.google.com/file/d/1uNp-c1f7LUw3pkSWDK-t6Jc_SkHbBFZv/view?usp=sharing" },  
+                 { name: "08 April Shift 1", url: "https://drive.google.com/file/d/1yvy0KodkGAjmCTUbX2E_vJDbrhMuPT-N/view?usp=sharing" },
+                { name: "08 April Shift 2", url: " https://drive.google.com/file/d/1jZj5bc4e5BVLUXyKsjA4nT2ujO56kd0w/view?usp=sharing" }, 
+                 { name: "09 April Shift 1", url: "https://drive.google.com/file/d/1cOQmcyUI7vEYsJrGpIv2NqCpSYLx57iI/view?usp=sharing" },
+                { name: "09 April Shift 2", url: "https://drive.google.com/file/d/143UBsH9eXBBVZQFRRPCO9fc2YN64lP7f/view?usp=sharing " }               
+            ],
+            "mains_2023": [
+                { name: "24 Jan Shift 1", url: " https://drive.google.com/file/d/1NX3HhXgH3YTXGxBYDGbCW9ns3nqh6BGm/view?usp=sharing" },
+                { name: "24 Jan Shift 2", url: "https://drive.google.com/file/d/1rnJlHFvOekUzGToWxVDPfy65bALnFmBd/view?usp=sharing " },
+                { name: "25 Jan Shift 1", url: "https://drive.google.com/file/d/1u7pzI_CG5wxk4ATM13WpW9VnYEk8mfzq/view?usp=sharing " },
+                { name: "25 Jan Shift 2", url: " https://drive.google.com/file/d/1riadG40KsFg-WPfbyNABJHFUmJWhh3WT/view?usp=sharing" },
+                { name: "29 Jan Shift 1", url: "https://drive.google.com/file/d/195iumEFsf4itt1OPr-ijkZ88n590_lk4/view?usp=sharing " },
+                { name: "29 Jan Shift 2", url: "https://drive.google.com/file/d/1OAcIjqe_4OjFhW3-qnBhqpzEZM8oBBaw/view?usp=sharing " },
+                { name: "30 Jan Shift 1", url: "https://drive.google.com/file/d/1vcbeA-FzTAVXwNGmAW5jqqI-bBeRqehi/view?usp=sharing " },
+                { name: "30 Jan Shift 2", url: " https://drive.google.com/file/d/1E00GYFFaxEmA_r8Pgeoj-DB2Y6WZgXsC/view?usp=sharing" },
+                { name: "31 Jan Shift 1", url: "https://drive.google.com/file/d/1synalfICoK-zTXrAPQLuOpv_qOhqrnLt/view?usp=sharing " },
+                { name: "31 Jan Shift 2", url: "https://drive.google.com/file/d/1dsBkzB20c_Owy6SZa3Ks4BMt45Jb8CEk/view?usp=sharing" },
+                { name: "01 Feb Shift 1", url: "https://drive.google.com/file/d/1DEjyJJnTahcikl-50E4h4B4ZJPL7bDRf/view?usp=sharing " },
+                { name: "01 Feb Shift 2", url: "https://drive.google.com/file/d/1xSRezQLvE977vrTUUxblErdXmrTE5Joh/view?usp=sharing " },
+                { name: "06 April Shift 1", url: "https://drive.google.com/file/d/12iXGdQcQ7mYxF8H9lYMVVaHYQ3jADw_g/view?usp=sharing " },
+                { name: "06 April Shift 2", url: "https://drive.google.com/file/d/1rHhh7SSGBIYOKF2wNqss9gTXAne-WVTn/view?usp=sharing " },
+                { name: "08 April Shift 1", url: "https://drive.google.com/file/d/16asbbgo5b4qHqwh8NKp4Ry4YZnjbMp78/view?usp=sharing " },
+                { name: "08 April Shift 2", url: " https://drive.google.com/file/d/19Jzeyq4wYWu2WYrPkaR7LdJlFUbNl8wb/view?usp=sharing" },
+                { name: "10 April Shift 1", url: "https://drive.google.com/file/d/1utjPfCkS7hOzuS1sUmiAbkIYkRL_w6Mx/view?usp=sharing " },
+                { name: "10 April Shift 2", url: "https://drive.google.com/file/d/1X7D2jUH2dbCJ68qqb5ew5kXf47JWq7sN/view?usp=sharing " },
+                { name: "11 April Shift 1", url: " https://drive.google.com/file/d/1YQYBahI9W1QS00Jy-97ITBILRJbQFIxI/view?usp=sharing" },
+                { name: "11 April Shift 2", url: "https://drive.google.com/file/d/1r7_A1UrivtcUqOsZXRzf5E3SfG08LEso/view?usp=sharing " },
+                { name: "12 April Shift 1", url: " https://drive.google.com/file/d/1aOdqhmJkqL1tMm45fihbFuwEyAVaVKzF/view?usp=sharing" },
+                { name: "13 April Shift 1", url: "https://drive.google.com/file/d/1XPhdr-XQLKg2bkKy-D7UShg-CUejm4Fv/view?usp=sharing " },
+                { name: "13 April Shift 2", url: "https://drive.google.com/file/d/1GgBvLpgHBCOJ_uyJs8Jwq7h8OcaJwWBA/view?usp=sharing " },
+                { name: "05 April Shift 1", url: " https://drive.google.com/file/d/1gNn22qDmJZ9u6FF4eaMl4U78jSqoNDBV/view?usp=sharing" }
+        ],
+        
+            "advanced_2022": [
+                { name: "Paper 1", url: "https://example.com/advanced_2022_paper1.pdf" },
+                { name: "Paper 2", url: "https://example.com/advanced_2022_paper2.pdf" },
+                { name: "Paper 3", url: "https://example.com/advanced_2022_paper3.pdf" }
+            ]
+        };
+
+        function showPapers() {
+            var exam = document.getElementById("exam").value;
+            var year = document.getElementById("year").value;
+            var linksContainer = document.getElementById("paperLinks");
+            var linksList = document.getElementById("linksList");
+
+            linksList.innerHTML = ""; // Clear previous links
+
+            if (exam && year) {
+                let key = exam + "_" + year;
+                if (paperLinksData[key]) {
+                    linksContainer.style.display = "block";
+                    paperLinksData[key].forEach(paper => {
+                        var paperLink = document.createElement("a");
+                        paperLink.href = paper.url;
+                        paperLink.target = "_blank";
+                        paperLink.className = "btn";
+                        paperLink.innerText = paper.name;
+                        linksList.appendChild(paperLink);
+                    });
+                } else {
+                    linksContainer.style.display = "none";
+                }
+            } else {
+                linksContainer.style.display = "none";
+            }
+        }
+    
